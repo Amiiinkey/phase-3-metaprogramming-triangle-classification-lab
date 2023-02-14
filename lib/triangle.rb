8263ad2040b411e0af57990a91c26b5a129184d2
Mohamed <aminito432@gmail.com>
2023-02-14 15:26:37 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
+
 class Triangle
-  # write code here
-end
+    class TriangleError < StandardError
+    end
+  
+    def initialize(side1, side2, side3)
+      @sides = [side1, side2, side3].sort
+      validate_triangle
+    end
+  
+    def kind
+      case @sides.uniq.size
+      when 1 then :equilateral
+      when 2 then :isosceles
+      else :scalene
+      end
+    end
+  
+    private
+  
+    def validate_triangle
+      raise TriangleError, "Illegal triangle" if @sides.any? { |s| s <= 0 }
+      raise TriangleError, "Triangle inequality violated" if @sides[0] + @sides[1] <= @sides[2]
+    end
+  end
+  
